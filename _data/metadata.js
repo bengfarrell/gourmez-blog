6274249ea943dd5d7bd2d645cbeff6e0e8dcebce
Ben Farrell <ben@benfarrell.com>
2024-09-10 19:08:15 -0700

    metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "The Gourmez",
+	url: "https://thegourmez.com",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Overthinking Food & Drink Since 2006",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Rebecca Gomez Farrell",
+		email: "becca@gomezfarrell.com",
+		url: "https://thegourmez.com"
 	}
 }
